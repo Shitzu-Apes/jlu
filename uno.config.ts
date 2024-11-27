0b78da0bfba54c6c4e564c64b2ece96d6012a8ee
Mario Reder <mario.reder@pm.me>
2024-11-27 11:32:16 +0100

    unocss prettier and eslint

---
+++ uno.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig, presetUno, presetAttributify, presetTypography, presetIcons } from 'unocss';
+
+export default defineConfig({
+	presets: [presetUno(), presetAttributify(), presetTypography(), presetIcons()]
+});
