f450b876a2795e203a6c5a16152e67bb98290de6
Mario Reder <mario.reder@pm.me>
2025-01-29 16:01:27 +0100

    first reproducible build

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ cd "`dirname $0`"
 
 cargo near build reproducible-wasm --manifest-path crates/token/Cargo.toml
 cargo near abi --manifest-path crates/token/Cargo.toml
-cargo build --release -p test-token --target wasm32-unknown-unknown
-cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 cp target/near/token/token.wasm ./res/
 cp target/near/token/token_abi.json ./res/
