be2567143879f70a57daf6d796cdc73bf15f03b1
r-near <163825889+r-near@users.noreply.github.com>
2025-02-06 16:09:02 -0800

    Fix amount issue

--- app/src/routes/bridge/+page.svelte
+++ app/src/routes/bridge/+page.svelte
@@ -173,8 +173,9 @@
 				const tokenAddress = omniAddress(ChainKind.Sol, import.meta.env.VITE_JLU_TOKEN_ID_SOLANA);
 
 				const fee = await api.getFee(sender, recipient, tokenAddress);
+				console.log(amount)
 				return client.initTransfer({
-					amount: BigInt(amount),
+					amount: BigInt(amount)/BigInt(1000000000n),
 					fee: BigInt(fee.transferred_token_fee ?? 0),
 					nativeFee: BigInt(fee.native_token_fee),
 					recipient,
@@ -189,9 +190,6 @@
 
 		console.log('[transferEvent]', transferEvent);
 		if (!transferEvent) {
-			throw new Error('Failed to initiate transfer');
-		}
-		if (typeof transferEvent === 'string') {
 			throw new Error('Failed to initiate transfer');
 		}
 
